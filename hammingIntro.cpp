35c3dad7dcbf90ab7f3676e31b245f484f9f2eb6
giacomovagni <giacomovagni@users.noreply.github.com>
2017-03-02 15:39:50 +0000

    Update hammingIntro.cpp

--- hammingIntro.cpp
+++ hammingIntro.cpp
@@ -12,22 +12,7 @@
 
 using namespace std;
 
-// generate random sequence of letter
-char randalpha(){
-    
-    int l = 10;
-    char alphabet[4] = {'a', 'b', 'c', 'd'};
-    
-    string seq[10] = { };
-    
-    for (int i = 0; i < l; ++i){
-        seq[i] = alphabet[ rand() % 4 ] ;
-    }
-
-    return 0;
-}
-
-
+// Hamming distanc 
 
 int main() {
     
